7cc5de3f205c8394a79ca736f3b8474b0384276a
Robyn-Dale Samuda <robyn@yuraki.com>
2017-09-20 06:28:34 -0500

    First commit

---
+++ src/components/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-for="day in days">{{ day }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        month: 9,
+        year: 2017
+      }
+    },
+    created() {
+      console.log(this.$moment);
+    },
+    computed: {
+      days() {
+        let days = [];
+        let currentDay = this.$moment(`${this.year}-${this.month}-1`, "YYYY-M-D");
+
+        do {
+          days.push(currentDay);
+          currentDay = this.$moment(currentDay).add(1, 'days');
+        } while ((currentDay.month() + 1) === this.month);
+        return days;
+      }
+    }
+  }
+</script>
--- src/web.entry.js
+++ src/web.entry.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import './style.scss';
+import moment from 'moment-timezone';
+moment.tz.setDefault('UTC');
+
+// Moment as an instance method
+// This as well as setting it in the data object allow all components to access the library
+Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment } });
+import App from './components/App.vue';
 
 new Vue({
   el: '#app',
   data: {
-    msg: 'Hello World'
+    moment
+  },
+  components: {
+    App
   }
 });
